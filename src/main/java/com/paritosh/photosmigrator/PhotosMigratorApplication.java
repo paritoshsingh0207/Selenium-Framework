34f9973747a4c5af97bf6b5e5670ed262b204b52
paritoshsingh0207 <43406364+paritoshsingh0207@users.noreply.github.com>
2026-09-07 23:06:35 +0530

    Route Takeout inventory command to local utility

--- src/main/java/com/paritosh/photosmigrator/PhotosMigratorApplication.java
+++ src/main/java/com/paritosh/photosmigrator/PhotosMigratorApplication.java
@@ -1,11 +1,17 @@
 package com.paritosh.photosmigrator;
 
+import com.paritosh.photosmigrator.local.LocalInventoryCli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhotosMigratorApplication {
     public static void main(String[] args) {
+        if (LocalInventoryCli.isInventoryCommand(args)) {
+            int exitCode = LocalInventoryCli.run(args);
+            if (exitCode != 0) System.exit(exitCode);
+            return;
+        }
         SpringApplication.run(PhotosMigratorApplication.class, args);
     }
 }
